1242b3f9e3aa76f1ce3da3441444f9958bc8f399
Alena Buiko <abuiko.ma@gmail.com>
2021-02-26 12:50:48 -0500

    change background/hex text when click on btn

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ const colorName = document.getElementById('color');
 const btn = document.querySelector(".btn");
 
 let letters = '0123456789ABCDEF';
-let hash = '#';
 let splitLetters = letters.split('');
 
-for (let i = 0; i < 6; i++) {
-    const random = randomNumber();
-    let color = splitLetters[random];
-    hash += color;
+
+btn.addEventListener('click', () => {
+    background.style.backgroundColor = generateHex();
+    colorName.innerHTML = generateHex();
+})
+
+function generateNumber() {
+    return Math.floor(Math.random() * 16);
 }
 
-function randomNumber() {
-    return Math.floor(Math.random() * 16);
+function generateHex() {
+    let hash = '#';
+    for (let i = 0; i < 6; i++) {
+        const randomNum = generateNumber();
+        let color = splitLetters[randomNum];
+        hash += color;
+    }
+    return hash;
 }
